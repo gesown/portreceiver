e2f5db73778cd6e3624132afca1fda96cd6c08bd
gesown <owner@groganenterpriseservices.com>
2019-05-15 06:04:30 -0700

    Initial publish

--- PortReceiver/Program.cs
+++ PortReceiver/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,19 @@ namespace PortReceiver
     {
         static void Main(string[] args)
         {
+            IList<byte> localBytes = new List<byte>();
+            var ipBytes = args[0].Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var ip in ipBytes)
+            {
+                localBytes.Add(byte.Parse(ip));
+            }
+
+            IPAddress localAddr = new IPAddress(localBytes.ToArray());
+            int localPort = int.Parse(args[1]);
+            TcpListener listener = new TcpListener(localAddr, localPort);
+            listener.Start();
+            Console.WriteLine("Port "+args[1]+" On IP "+args[0]+" Listening");
+            Console.Read();
         }
     }
 }
